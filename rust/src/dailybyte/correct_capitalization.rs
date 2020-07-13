d61ece98da9cdfa91652552e875147848a2b8ed7
Sam Tay <sam.chong.tay@gmail.com>
2020-07-13 00:43:59 -0700

    Solve the fourth daily byte

---
+++ rust/src/dailybyte/correct_capitalization.rs
@@ -0,0 +1,32 @@
+//! This question is asked by Google. Given a string, return whether or not it uses capitalization
+//! correctly. A string correctly uses capitalization if all letters are capitalized, no letters are
+//! capitalized, or only the first letter is capitalized.
+
+fn is_correct<S>(s: S) -> bool
+where
+    S: Into<String>,
+{
+    let s = s.into();
+    let mut cs = s.chars();
+    match (
+        cs.next().map(|c| c.is_uppercase()),
+        cs.next().map(|c| c.is_uppercase()),
+    ) {
+        (Some(true), Some(true)) => cs.all(|c| c.is_uppercase()),
+        (Some(true), Some(false)) | (Some(false), Some(false)) => cs.all(|c| c.is_lowercase()),
+        _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vacuum_position() {
+        assert!(is_correct("USA"));
+        assert!(is_correct("Calvin"));
+        assert!(!is_correct("compUter"));
+        assert!(is_correct("coding"));
+    }
+}
--- rust/src/dailybyte/mod.rs
+++ rust/src/dailybyte/mod.rs
@@ -1,3 +1,4 @@
+pub mod correct_capitalization;
 pub mod reverse_string;
 pub mod vacuum_route;
 pub mod valid_palindrome;
