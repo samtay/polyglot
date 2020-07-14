535e5cd2e3013e037fd83e63ce23e3cd17a7fc36
Sam Tay <sam.chong.tay@gmail.com>
2020-07-14 15:11:03 -0700

    Solve the sixth daily byte

--- rust/src/dailybyte/mod.rs
+++ rust/src/dailybyte/mod.rs
@@ -1,5 +1,6 @@
 pub mod add_binary;
 pub mod correct_capitalization;
+pub mod prefix;
 pub mod reverse_string;
 pub mod vacuum_route;
 pub mod valid_palindrome;
---
+++ rust/src/dailybyte/prefix.rs
@@ -0,0 +1,41 @@
+//! This question is asked by Microsoft. Given an array of strings, return the longest common prefix
+//! that is shared amongst all strings.  Note: you may assume all strings only contain lowercase
+//! alphabetical characters.
+
+fn longest_prefix<'a>(strings: &[&'a str]) -> &'a str {
+    let mut length = 0;
+    loop {
+        let ixs = strings.iter().map(|s| s.chars().nth(length));
+        if all_equal(ixs) {
+            length += 1;
+        } else {
+            break;
+        }
+    }
+    &strings.first().unwrap_or(&"")[..length]
+}
+
+fn all_equal<I>(list: I) -> bool
+where
+    I: IntoIterator,
+    I::Item: Eq,
+{
+    let mut list = list.into_iter();
+    if let Some(first) = list.nth(0) {
+        list.all(|item| item == first)
+    } else {
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prefix() {
+        assert_eq!(longest_prefix(&["colorado", "color", "cold"]), "col");
+        assert_eq!(longest_prefix(&["a", "b", "c"]), "");
+        assert_eq!(longest_prefix(&["spot", "spotty", "spotted"]), "spot");
+    }
+}
