98d1fc628379c74f6adb578bb6a5351513b7ca7a
Sam Tay <sam.chong.tay@gmail.com>
2020-07-17 10:30:58 -0700

    Use my own binary search

--- rust/src/dailybyte/two_sum.rs
+++ rust/src/dailybyte/two_sum.rs
@@ -3,6 +3,8 @@
 //!
 //! Note: you may not sum a number with itself.
 
+use std::cmp::Ordering;
+
 /// Note: given an arbitrary unordered list, we'd have to check each unique pair, of which there
 /// are (n choose 2), and thus O(n^2).
 ///
@@ -16,12 +18,43 @@ fn two_sum(mut ints: Vec<i32>, sum: i32) -> bool {
     for i in 0..ints.len() {
         let target = sum - ints[i];
         let (_, the_rest) = ints.split_at(i + 1);
-        if let Ok(_) = the_rest.binary_search(&target) {
-            println!("SUCCESS");
+        if binary_search(the_rest, &target) {
             return true;
         }
     }
     false
+}
+
+fn binary_search<T: Ord>(xs: &[T], x: &T) -> bool {
+    // Handle empty case
+    let len = xs.len();
+    if len == 0 {
+        return false;
+    }
+    // Initial binary search parameters
+    let mut min = 0;
+    let mut max = xs.len() - 1;
+    loop {
+        let mid = (max + min) / 2;
+        match xs[mid].cmp(x) {
+            Ordering::Equal => return true,
+            Ordering::Greater => {
+                if mid == 0 {
+                    return false;
+                }
+                max = mid - 1;
+            }
+            Ordering::Less => {
+                if mid == len {
+                    return false;
+                }
+                min = mid + 1;
+            }
+        }
+        if min > max {
+            return false;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -33,5 +66,19 @@ mod tests {
         assert!(two_sum(vec![1, 3, 8, 2], 10));
         assert!(!two_sum(vec![3, 9, 13, 7], 8));
         assert!(two_sum(vec![4, 2, 6, 5, 2], 4));
+    }
+
+    #[test]
+    fn test_binary_search() {
+        assert!(binary_search(&[1, 2, 3, 5, 8, 13, 21, 34, 55, 89], &1));
+        assert!(binary_search(&[1, 2, 3, 5, 8, 13, 21, 34, 55, 89], &2));
+        assert!(binary_search(&[1, 2, 3, 5, 8, 13, 21, 34, 55, 89], &89));
+        assert!(!binary_search(&[1, 2, 3, 5, 8, 13, 21, 34, 55, 89], &90));
+        assert!(binary_search(&[1, 2, 3, 5, 8, 13, 21, 34, 55, 89], &13));
+        assert!(!binary_search(&[1, 2, 3, 5, 8, 13, 21, 34, 55, 89], &14));
+        assert!(!binary_search(
+            &[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 90],
+            &14
+        ));
     }
 }
