22df1ccf3da32423b1aa6303a4f48cc13285ae79
Sam Tay <samctay@pm.me>
2022-06-13 22:35:33 -0700

    Fix triangle problem with DP

--- rust/src/leet/triangle.rs
+++ rust/src/leet/triangle.rs
@@ -1,26 +1,45 @@
-#[derive(Debug, Clone, PartialEq)]
-pub struct Candidate {
-    pub running_total: i32,
-    pub col_index: usize,
-}
+use std::cmp;
+
 pub struct Solution();
 impl Solution {
-    /// Runs in O(2^n) time and O(2^n) space
-    ///
-    /// To do this in O(n) space, the approach would change from going through
-    /// all paths, and then taking a minimum across all of them, to going
-    /// through paths one at a time, keeping track of a single minimum. Then the
-    /// only space allocation that is necessary is a single path.
+    /// Notice the overlapping structure and leverage Dynamic Programming.
+    /// The minimum total for a given entry T[r, c] is
+    ///     T[r, c] + min(T[r - 1, c], T[r - 1, c + 1])
     pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
+        // The minimum totals of the single element triangles in the bottom row are trivial
+        let mut triangle = triangle;
+        let mut min_tots = triangle.pop().unwrap(); // safe given constraints
+
+        // Then, go up the triangle, and make the next row of minimum totals
+        triangle.reverse();
+        for row in triangle {
+            let mut temp = Vec::with_capacity(row.len());
+            for (col_ix, col) in row.into_iter().enumerate() {
+                temp.push(col + cmp::min(min_tots[col_ix], min_tots[col_ix + 1]));
+            }
+            min_tots = temp;
+        }
+
+        // The final, single element row is the actual minimum total
+        *min_tots.last().unwrap() // safe given constraints
+    }
+
+    /// Runs in O(2^n) time and O(2^n) space.
+    pub fn minimum_total_bad(triangle: Vec<Vec<i32>>) -> i32 {
+        #[derive(Debug, Clone, PartialEq)]
+        pub struct Candidate {
+            pub running_total: i32,
+            pub col_index: usize,
+        }
         // Since the array is not sorted, we'll have to do an exhaustive search
         // Although a sorted one would have a very trivial solution :)
         // Each step has two choices:
         let height = triangle.len();
         let total_candidates = 2 ^ (height - 1);
 
-        // Guaranteed 1 <= triangle.length
         let mut candidates: Vec<Candidate> = Vec::with_capacity(total_candidates);
         let mut triangle_iter = triangle.iter();
+        // Guaranteed 1 <= triangle.length
         let top_vertex = triangle_iter.next().unwrap().into_iter().next().unwrap();
         candidates.push(Candidate {
             running_total: *top_vertex,
@@ -67,6 +86,8 @@ mod tests {
             11
         );
     }
+
+    #[test]
     fn test_triangle_performance() {
         assert_eq!(
             Solution::minimum_total(vec![
